342e7b6a0617af1b225817dd5b56084d139cfa63
Thibaut Cuvelier <cuvelier.thibaut@gmail.com>
2021-03-04 13:21:22 +0100

    Uniformise set names.

--- src/sets_reification.jl
+++ src/sets_reification.jl
@@ -1,5 +1,5 @@
 """
-    ReificationSet{S <: MOI.AbstractSet}(set::S)
+    Reified{S <: MOI.AbstractSet}(set::S)
 
 ``\\{(y, x) \\in \\{0, 1\\} \\times \\mathbb{R}^n | y = 1 \\iff x \\in set, y = 0 otherwise\\}``.
 
@@ -7,15 +7,15 @@ This set serves to find out whether a given constraint is satisfied.
 
 The only possible values are 0 and 1 for the first variable of the set.
 """
-struct ReificationSet{S <: MOI.AbstractSet} <: MOI.AbstractVectorSet
+struct Reified{S <: MOI.AbstractSet} <: MOI.AbstractVectorSet
     set::S
 end
 
-MOI.dimension(set::ReificationSet{S}) where {S} = 1 + MOI.dimension(set.set)
-Base.copy(set::ReificationSet{S}) where {S} = ReificationSet(copy(set.set))
+MOI.dimension(set::Reified{S}) where {S} = 1 + MOI.dimension(set.set)
+Base.copy(set::Reified{S}) where {S} = Reified(copy(set.set))
 
 """
-    EquivalenceSet{S1 <: MOI.AbstractSet, S2 <: MOI.AbstractSet}(set1::S1, 
+    Equivalence{S1 <: MOI.AbstractSet, S2 <: MOI.AbstractSet}(set1::S1, 
                                                                  set2::S2)
 
 The logical equivalence operator ≡ or ⇔.
@@ -27,44 +27,44 @@ More explicitly, if the first one is satisfied, then the second one is implied
 to be satisfied too; if the second one is satisfied, then the first one is 
 implied.
 """
-struct EquivalenceSet{S1 <: MOI.AbstractSet, S2 <: MOI.AbstractSet} <:
+struct Equivalence{S1 <: MOI.AbstractSet, S2 <: MOI.AbstractSet} <:
        MOI.AbstractVectorSet
     set1::S1
     set2::S2
 end
 
-function MOI.dimension(set::EquivalenceSet{S, T}) where {S, T}
+function MOI.dimension(set::Equivalence{S, T}) where {S, T}
     return MOI.dimension(set.set1) + MOI.dimension(set.set2)
 end
 
-function Base.copy(set::EquivalenceSet{S, T}) where {S, T}
-    return EquivalenceSet(copy(set.set), copy(set.set2))
+function Base.copy(set::Equivalence{S, T}) where {S, T}
+    return Equivalence(copy(set.set), copy(set.set2))
 end
 
 """
-EquivalenceNotSet{S1 <: MOI.AbstractSet, S2 <: MOI.AbstractSet}(set1::S1, 
+EquivalenceNot{S1 <: MOI.AbstractSet, S2 <: MOI.AbstractSet}(set1::S1, 
                                                                 set2::S2)
 
 The logical equivalence operator ≡ or ⇔, with the second argument negated.
 
 ``\\{(x, y) \\in \\mathbb{R}^{a+b} | x \\in S1 \\iff y \\not\\in S2\\}``.
 """
-struct EquivalenceNotSet{S1 <: MOI.AbstractSet, S2 <: MOI.AbstractSet} <:
+struct EquivalenceNot{S1 <: MOI.AbstractSet, S2 <: MOI.AbstractSet} <:
        MOI.AbstractVectorSet
     set1::S1
     set2::S2
 end
 
-function MOI.dimension(set::EquivalenceNotSet{S, T}) where {S, T}
+function MOI.dimension(set::EquivalenceNot{S, T}) where {S, T}
     return MOI.dimension(set.set1) + MOI.dimension(set.set2)
 end
 
-function Base.copy(set::EquivalenceNotSet{S, T}) where {S, T}
-    return EquivalenceNotSet(copy(set.set), copy(set.set2))
+function Base.copy(set::EquivalenceNot{S, T}) where {S, T}
+    return EquivalenceNot(copy(set.set), copy(set.set2))
 end
 
 """
-    IfThenElseSet{
+    IfThenElse{
         Condition <: MOI.AbstractSet, 
         TrueConstraint <: MOI.AbstractSet, 
         FalseConstraint <: MOI.AbstractSet
@@ -79,7 +79,7 @@ If the `condition` is satisfied, then the first constraint (of type
 
 ``\\{(x, y, z) \\in \\mathbb{R}^(a+b+c) | y \\in TrueConstraint \\iff x \\in set, z \\in FalseConstraint otherwise\\}``.
 """
-struct IfThenElseSet{
+struct IfThenElse{
     Condition <: MOI.AbstractSet,
     TrueConstraint <: MOI.AbstractSet,
     FalseConstraint <: MOI.AbstractSet,
@@ -89,14 +89,14 @@ struct IfThenElseSet{
     false_constraint::FalseConstraint
 end
 
-function MOI.dimension(set::IfThenElseSet{S, T, U}) where {S, T, U}
+function MOI.dimension(set::IfThenElse{S, T, U}) where {S, T, U}
     return MOI.dimension(set.condition) +
            MOI.dimension(set.true_constraint) +
            MOI.dimension(set.false_constraint)
 end
 
-function Base.copy(set::IfThenElseSet{S, T, U}) where {S, T, U}
-    return ReificationSet(
+function Base.copy(set::IfThenElse{S, T, U}) where {S, T, U}
+    return Reified(
         copy(set.condition),
         copy(set.true_constraint),
         copy(set.false_constraint),
@@ -104,7 +104,7 @@ function Base.copy(set::IfThenElseSet{S, T, U}) where {S, T, U}
 end
 
 """
-    ImplySet{
+    Imply{
         Antecedent <: MOI.AbstractSet,
         Consequent <: MOI.AbstractSet
     }(antecedent::Antecedent, consequent::Consequent)
@@ -116,7 +116,7 @@ satisfied. Otherwise, nothing is implied on the truth value of `consequent`.
 
 ``\\{(x, y) \\in \\mathbb{R}^a \\times \\mathbb{R}^b | y \\in Consequent if x \\in Antecedent\\}``.
 """
-struct ImplySet{
+struct Imply{
     Antecedent <: MOI.AbstractSet,
     Consequent <: MOI.AbstractSet
 } <: MOI.AbstractVectorSet
@@ -124,40 +124,40 @@ struct ImplySet{
     consequent::Consequent
 end
 
-function MOI.dimension(set::ImplySet{S, T}) where {S, T}
+function MOI.dimension(set::Imply{S, T}) where {S, T}
     return MOI.dimension(set.antecedent) +
            MOI.dimension(set.consequent)
 end
 
-function Base.copy(set::ImplySet{S, T}) where {S, T}
-    return ReificationSet(
+function Base.copy(set::Imply{S, T}) where {S, T}
+    return Reified(
         copy(set.antecedent),
         copy(set.consequent),
     )
 end
 
 """
-    TrueSet()
+    True()
 
 A constraint that is always true. 
 
 It is only useful with reification-like constraints.
 """
-struct TrueSet <: MOI.AbstractVectorSet
+struct True <: MOI.AbstractVectorSet
 end
 
-MOI.dimension(set::TrueSet) = 0
-Base.copy(set::TrueSet) = set
+MOI.dimension(set::True) = 0
+Base.copy(set::True) = set
 
 """
-    FalseSet()
+    False()
 
 A constraint that is always false. 
 
 It is only useful with reification-like constraints.
 """
-struct FalseSet <: MOI.AbstractVectorSet
+struct False <: MOI.AbstractVectorSet
 end
 
-MOI.dimension(set::FalseSet) = 0
-Base.copy(set::FalseSet) = set
+MOI.dimension(set::False) = 0
+Base.copy(set::False) = set
