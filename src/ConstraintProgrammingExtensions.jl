c830319c050fbe1d34b65ac8995879a27a327e4d
Thibaut Cuvelier <dourouc05@gmail.com>
2021-03-11 19:21:20 +0100

    Split the sets in several files.

--- src/ConstraintProgrammingExtensions.jl
+++ src/ConstraintProgrammingExtensions.jl
@@ -5,6 +5,8 @@ const MOI = MathOptInterface
 const MOIU = MOI.Utilities
 
 include("sets.jl") # Most sets.
+include("sets_sorting.jl") # Sets related to sorting values.
+include("sets_strictly.jl") # Strictly is defined for many of the above sets.
 include("sets_reification.jl") # Sets that rely on the concept of reification.
 
 end
---
+++ src/sets_strictly.jl
@@ -0,0 +1,50 @@
+"""
+    Strictly{S <: Union{LessThan{T}, GreaterThan{T}, LexicographicallyGreaterThan}}
+
+Converts an inequality set to a set with the same inequality made strict.
+For example, while `LessThan(1)` corresponds to the inequality `x <= 1`,
+`Strictly(LessThan(1))` corresponds to the inequality `x < 1`.
+
+## Example
+
+    x in Strictly(LessThan(1))
+"""
+struct Strictly{
+    S <: Union{
+        MOI.LessThan{T} where T,
+        MOI.GreaterThan{T} where T,
+        LexicographicallyLessThan,
+        LexicographicallyGreaterThan,
+        Increasing,
+        Decreasing,
+    },
+    T <: Number,
+} <: MOI.AbstractScalarSet
+    set::S
+end
+
+Base.copy(set::Strictly{S}) where {S} = Count(copy(set.set))
+MOI.constant(set::Strictly{S}) where {S} = MOI.constant(set.set)
+MOI.dimension(set::Strictly{S}) where {S} = MOI.dimension(set.set)
+function MOIU.shift_constant(set::Strictly{S}, offset::T) where {S, T}
+    return typeof(set)(MOIU.shift_constant(set.set, offset))
+end
+
+function Strictly(set::LexicographicallyLessThan)
+    return Strictly{LexicographicallyLessThan, Int}(set)
+end
+function Strictly(set::LexicographicallyGreaterThan)
+    return Strictly{LexicographicallyGreaterThan, Int}(set)
+end
+function Strictly(set::MOI.LessThan{T}) where {T} 
+    return Strictly{MOI.LessThan{T}, T}(set)
+end
+function Strictly(set::MOI.GreaterThan{T}) where {T}
+    return Strictly{MOI.GreaterThan{T}, T}(set)
+end
+function Strictly(set::Increasing)
+    return Strictly{Increasing, Int}(set)
+end
+function Strictly(set::Decreasing)
+    return Strictly{Decreasing, Int}(set)
+end
