08f54f1f1acdb0a195d9005499b3d7ff5b7de0cb
Thibaut Cuvelier <dourouc05@gmail.com>
2021-03-11 19:47:01 +0100

    Add Cumulative.

--- src/ConstraintProgrammingExtensions.jl
+++ src/ConstraintProgrammingExtensions.jl
@@ -6,7 +6,8 @@ const MOIU = MOI.Utilities
 
 include("sets.jl") # Most sets.
 include("sets_sorting.jl") # Sets related to sorting values.
-include("sets_strictly.jl") # Strictly is defined for many of the above sets.
+include("sets_strictly.jl") # Strictly is defined for many of the above sets (sets.jl and sets_sorting.jl).
+include("sets_scheduling.jl") # Sets related to scheduling.
 include("sets_reification.jl") # Sets that rely on the concept of reification.
 
 end
---
+++ src/sets_scheduling.jl
@@ -0,0 +1,32 @@
+"""
+    CumulativeResource(n_tasks::Int)
+
+Each task is given by a minimum start time (the first `n_tasks` variables), 
+a duration (the next `n_tasks` variables), and the resource consumption 
+(the following `n_tasks` variables). The final variable is the maximum amount of
+the resource available.
+
+Also called [`cumulative`](https://sofdem.github.io/gccat/gccat/Ccumulative.html).
+This version does not consider end deadlines for tasks.
+"""
+struct CumulativeResource <: MOI.AbstractVectorSet
+    n_tasks::Int
+end
+
+MOI.dimension(set::CumulativeResource) = 3 * set.n_tasks + 1
+
+"""
+    CumulativeResourceWithDeadline(n_tasks::Int)
+
+Each task is given by a minimum start time (the first `n_tasks` variables), 
+a duration (the next `n_tasks` variables), a deadline (the following `n_tasks` 
+variables), and the resource consumption (the next `n_tasks` variables). 
+The final variable is the maximum amount of the resource available.
+
+Also called [`cumulative`](https://sofdem.github.io/gccat/gccat/Ccumulative.html).
+"""
+struct CumulativeResourceWithDeadline <: MOI.AbstractVectorSet
+    n_tasks::Int
+end
+
+MOI.dimension(set::CumulativeResource) = 4 * set.n_tasks + 1
