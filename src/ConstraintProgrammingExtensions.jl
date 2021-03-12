2886312918a295e5629cc4441ffeff29ae01c06b
Thibaut Cuvelier <dourouc05@gmail.com>
2021-03-12 21:45:51 +0100

    Add Circuit.

--- src/ConstraintProgrammingExtensions.jl
+++ src/ConstraintProgrammingExtensions.jl
@@ -8,6 +8,7 @@ include("sets.jl") # Most sets.
 include("sets_sorting.jl") # Sets related to sorting values.
 include("sets_strictly.jl") # Strictly is defined for many of the above sets (sets.jl and sets_sorting.jl).
 include("sets_scheduling.jl") # Sets related to scheduling.
+include("sets_graph.jl") # Sets that work on graphs.
 include("sets_reification.jl") # Sets that rely on the concept of reification.
 
 end
---
+++ src/sets_graph.jl
@@ -0,0 +1,31 @@
+"""
+    Circuit(n_nodes::Int)
+
+A Hamiltonian circuit. If the vector `x` is constrained within a `Circuit(n)`,
+each `x[i]` denotes the next node in the graph, for `i ∈ [1, n]`. The 
+considered graph is an undirected complete graph with `n` nodes.
+
+Also called `cycle` or `atour`.
+"""
+struct Circuit <: MOI.AbstractVectorSet
+    n_nodes::Int
+end
+
+MOI.dimension(set::Circuit) = set.n_nodes
+
+"""
+    WeightedCircuit{T <: Real}(Circuit::Int)
+
+A Hamiltonian circuit. If the vector `x` and the scalar `c` are constrained
+within a `WeightedCircuit(n, cost_matrix)`, each `x[i]` denotes the next node 
+in the graph, for `i ∈ [1, n]`. The considered graph is an undirected complete 
+graph with `n` nodes. `c` is the total cost of the circuit, defined as: 
+
+``c = \sum_{i=1}^n \\mathtt{cost_matrix}_{i, x[i]}``
+"""
+struct WeightedCircuit{T <: Real} <: MOI.AbstractVectorSet
+    n_nodes::Int
+    cost_matrix::AbstractMatrix{T}
+end
+
+MOI.dimension(set::Circuit) = set.n_nodes + 1
