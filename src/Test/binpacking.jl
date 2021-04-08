07028ae147730d10a5f8a30b70adc4ee7cfc49af
Thibaut Cuvelier <dourouc05@gmail.com>
2021-04-09 01:44:36 +0200

    Migrate bin packing too. Missing includes.

--- src/Test/binpacking.jl
+++ src/Test/binpacking.jl
@@ -1,47 +1,4 @@
-@testset "BinPacking: ScalarAffineFunction" begin
-    model = OPTIMIZER
-    MOI.empty!(model)
-
-    @test MOI.supports_constraint(model, MOI.SingleVariable, MOI.Integer)
-    @test MOI.supports_constraint(
-        model,
-        MOI.ScalarAffineFunction{Int},
-        MOI.EqualTo{Int},
-    )
-    @test MOI.supports_constraint(
-        model,
-        MOI.VectorAffineFunction{Int},
-        CP.BinPacking{Int},
-    )
-
-    x1, _ = MOI.add_constrained_variable(model, MOI.Integer())
-    x2, _ = MOI.add_constrained_variable(model, MOI.Integer())
-    x3, _ = MOI.add_constrained_variable(model, MOI.Integer())
-    w1 = 2
-    w2 = 2
-
-    c1 = MOI.add_constraint(
-        model,
-        _vaf([x1, x2, x3]),
-        CP.BinPacking(1, 2, [w1, w2]),
-    )
-
-    @test MOI.is_valid(model, x1)
-    @test MOI.is_valid(model, x2)
-    @test MOI.is_valid(model, x3)
-    @test MOI.is_valid(model, c1)
-
-    MOI.optimize!(model)
-    @test MOI.get(model, MOI.TerminationStatus()) == MOI.OPTIMAL
-    @test MOI.get(model, MOI.PrimalStatus()) == MOI.FEASIBLE_POINT
-
-    @test MOI.get(model, MOI.ResultCount()) >= 1
-    @test MOI.get(model, MOI.VariablePrimal(), x1) == 4
-    @test MOI.get(model, MOI.VariablePrimal(), x2) == 0
-    @test MOI.get(model, MOI.VariablePrimal(), x3) == 0
-end
-
-@testset "BinPacking: VectorOfVariables" begin
+function binpacking_vectorofvariables_test(model::MOI.ModelLike, config::MOIT.TestConfig)
     model = OPTIMIZER
     MOI.empty!(model)
 
@@ -74,14 +31,61 @@ end
     @test MOI.is_valid(model, x3)
     @test MOI.is_valid(model, c1)
 
-    MOI.optimize!(model)
-    @test MOI.get(model, MOI.TerminationStatus()) == MOI.OPTIMAL
-    @test MOI.get(model, MOI.PrimalStatus()) == MOI.FEASIBLE_POINT
+    if config.solve
+        MOI.optimize!(model)
+        @test MOI.get(model, MOI.TerminationStatus()) == MOI.OPTIMAL
+        @test MOI.get(model, MOI.PrimalStatus()) == MOI.FEASIBLE_POINT
 
-    @test MOI.get(model, MOI.ResultCount()) >= 1
-    @test MOI.get(model, MOI.VariablePrimal(), x1) == 4
-    @test MOI.get(model, MOI.VariablePrimal(), x2) == 0
-    @test MOI.get(model, MOI.VariablePrimal(), x3) == 0
+        @test MOI.get(model, MOI.ResultCount()) >= 1
+        @test MOI.get(model, MOI.VariablePrimal(), x1) == 4
+        @test MOI.get(model, MOI.VariablePrimal(), x2) == 0
+        @test MOI.get(model, MOI.VariablePrimal(), x3) == 0
+    end
+end
+
+function binpacking_scalaraffinefunction_test(model::MOI.ModelLike, config::MOIT.TestConfig)
+    model = OPTIMIZER
+    MOI.empty!(model)
+
+    @test MOI.supports_constraint(model, MOI.SingleVariable, MOI.Integer)
+    @test MOI.supports_constraint(
+        model,
+        MOI.ScalarAffineFunction{Int},
+        MOI.EqualTo{Int},
+    )
+    @test MOI.supports_constraint(
+        model,
+        MOI.VectorAffineFunction{Int},
+        CP.BinPacking{Int},
+    )
+
+    x1, _ = MOI.add_constrained_variable(model, MOI.Integer())
+    x2, _ = MOI.add_constrained_variable(model, MOI.Integer())
+    x3, _ = MOI.add_constrained_variable(model, MOI.Integer())
+    w1 = 2
+    w2 = 2
+
+    c1 = MOI.add_constraint(
+        model,
+        _vaf([x1, x2, x3]),
+        CP.BinPacking(1, 2, [w1, w2]),
+    )
+
+    @test MOI.is_valid(model, x1)
+    @test MOI.is_valid(model, x2)
+    @test MOI.is_valid(model, x3)
+    @test MOI.is_valid(model, c1)
+
+    if config.solve
+        MOI.optimize!(model)
+        @test MOI.get(model, MOI.TerminationStatus()) == MOI.OPTIMAL
+        @test MOI.get(model, MOI.PrimalStatus()) == MOI.FEASIBLE_POINT
+
+        @test MOI.get(model, MOI.ResultCount()) >= 1
+        @test MOI.get(model, MOI.VariablePrimal(), x1) == 4
+        @test MOI.get(model, MOI.VariablePrimal(), x2) == 0
+        @test MOI.get(model, MOI.VariablePrimal(), x3) == 0
+    end
 end
 
 # @testset "BinPacking: ScalarAffineFunction with variable sizes" begin
@@ -109,3 +113,10 @@ end
 #                            CP.BinPacking(1, 2))
 #     )
 # end
+
+const binpackingtests = Dict(
+    "binpacking_vectorofvariables" => binpacking_vectorofvariables_test,
+    "binpacking_scalaraffinefunction" => binpacking_scalaraffinefunction_test,
+)
+
+@MOIT.moitestset binpacking
