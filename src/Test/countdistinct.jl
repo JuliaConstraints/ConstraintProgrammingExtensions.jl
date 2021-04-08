9561a2e4b3ba91c15f7b76c73e838e10b3b9dbe5
Thibaut Cuvelier <dourouc05@gmail.com>
2021-04-09 01:13:29 +0200

    Create countdistinct.jl

---
+++ src/Test/countdistinct.jl
@@ -0,0 +1,98 @@
+function countdistinct_vectorofvariables_test(model::MOI.ModelLike, config::MOIT.TestConfig)
+    MOI.empty!(model)
+
+    @test MOI.supports_constraint(model, MOI.SingleVariable, MOI.Integer)
+    @test MOI.supports_constraint(model, MOI.SingleVariable, MOI.EqualTo{Int})
+    @test MOI.supports_constraint(
+        model,
+        MOI.VectorOfVariables,
+        CP.CountDistinct,
+    )
+
+    x1, _ = MOI.add_constrained_variable(model, MOI.Integer())
+    x2, _ = MOI.add_constrained_variable(model, MOI.Integer())
+    x3, _ = MOI.add_constrained_variable(model, MOI.Integer())
+    x4, _ = MOI.add_constrained_variable(model, MOI.Integer())
+
+    c1 = MOI.add_constraint(model, x1, MOI.EqualTo(1))
+    c2 = MOI.add_constraint(model, x2, MOI.EqualTo(1))
+    c3 = MOI.add_constraint(model, x3, MOI.EqualTo(2))
+
+    c4 = MOI.add_constraint(model, _vov([x4, x1, x2, x3]), CP.CountDistinct(3))
+
+    @test MOI.is_valid(model, x1)
+    @test MOI.is_valid(model, x2)
+    @test MOI.is_valid(model, x3)
+    @test MOI.is_valid(model, x4)
+    @test MOI.is_valid(model, c1)
+    @test MOI.is_valid(model, c2)
+    @test MOI.is_valid(model, c3)
+    @test MOI.is_valid(model, c4)
+
+    if config.solve
+        MOI.optimize!(model)
+        @test MOI.get(model, MOI.TerminationStatus()) == MOI.OPTIMAL
+        @test MOI.get(model, MOI.PrimalStatus()) == MOI.FEASIBLE_POINT
+
+        @test MOI.get(model, MOI.ResultCount()) >= 1
+        @test MOI.get(model, MOI.VariablePrimal(), x1) == 1
+        @test MOI.get(model, MOI.VariablePrimal(), x2) == 1
+        @test MOI.get(model, MOI.VariablePrimal(), x3) == 2
+        @test MOI.get(model, MOI.VariablePrimal(), x4) == 2
+    end
+end
+
+function countdistinct_vectoraffinefunction_test(model::MOI.ModelLike, config::MOIT.TestConfig)
+    MOI.empty!(model)
+
+    @test MOI.supports_constraint(model, MOI.SingleVariable, MOI.Integer)
+    @test MOI.supports_constraint(
+        model,
+        MOI.ScalarAffineFunction{Int},
+        MOI.EqualTo{Int},
+    )
+    @test MOI.supports_constraint(
+        model,
+        MOI.VectorAffineFunction{Int},
+        CP.CountDistinct,
+    )
+
+    x1, _ = MOI.add_constrained_variable(model, MOI.Integer())
+    x2, _ = MOI.add_constrained_variable(model, MOI.Integer())
+    x3, _ = MOI.add_constrained_variable(model, MOI.Integer())
+    x4, _ = MOI.add_constrained_variable(model, MOI.Integer())
+
+    c1 = MOI.add_constraint(model, _saf([x1]), MOI.EqualTo(1))
+    c2 = MOI.add_constraint(model, _saf([x2]), MOI.EqualTo(1))
+    c3 = MOI.add_constraint(model, _saf([x3]), MOI.EqualTo(2))
+
+    c4 = MOI.add_constraint(model, _vaf([x4, x1, x2, x3]), CP.CountDistinct(3))
+
+    @test MOI.is_valid(model, x1)
+    @test MOI.is_valid(model, x2)
+    @test MOI.is_valid(model, x3)
+    @test MOI.is_valid(model, x4)
+    @test MOI.is_valid(model, c1)
+    @test MOI.is_valid(model, c2)
+    @test MOI.is_valid(model, c3)
+    @test MOI.is_valid(model, c4)
+
+    if config.solve
+        MOI.optimize!(model)
+        @test MOI.get(model, MOI.TerminationStatus()) == MOI.OPTIMAL
+        @test MOI.get(model, MOI.PrimalStatus()) == MOI.FEASIBLE_POINT
+
+        @test MOI.get(model, MOI.ResultCount()) >= 1
+        @test MOI.get(model, MOI.VariablePrimal(), x1) == 1
+        @test MOI.get(model, MOI.VariablePrimal(), x2) == 1
+        @test MOI.get(model, MOI.VariablePrimal(), x3) == 2
+        @test MOI.get(model, MOI.VariablePrimal(), x4) == 2
+    end
+end
+
+const countdistincttests = Dict(
+    "countdistinct_vectorofvariables" => countdistinct_vectorofvariables_test,
+    "countdistinct_vectoraffinefunction" => countdistinct_vectoraffinefunction_test,
+)
+
+@MOIT.moitestset countdistinct
