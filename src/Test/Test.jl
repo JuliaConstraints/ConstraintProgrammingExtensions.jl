00f60fe1d373eddd63805498e123ffeb563f0b09
Thibaut Cuvelier <dourouc05@gmail.com>
2021-04-09 00:39:48 +0200

    Start migrating tests from CPLEX to COIT.

---
+++ src/Test/Test.jl
@@ -0,0 +1,38 @@
+module Test
+
+import Test
+import MathOptInterface
+
+const MOI = MathOptInterface
+const MOIU = MOI.Utilities
+const MOIT = MOI.Test
+
+# A few helpers
+function _vov(vars::Vector{MOI.VariableIndex})
+    return MOI.VectorOfVariables(vars)
+end
+
+function _saf(var::MOI.VariableIndex)
+    return MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.([1], [var]), 0)
+end
+
+function _saf(vars::Vector{MOI.VariableIndex})
+    return _saf(ones(Int, length(vars)), vars)
+end
+
+function _saf(coeffs::Vector{Int}, vars::Vector{MOI.VariableIndex})
+    @assert length(coeffs) == length(vars)
+    return MOI.ScalarAffineFunction(MOI.ScalarAffineTerm.(coeffs, vars), 0)
+end
+
+function _vaf(vars::Vector{MOI.VariableIndex})
+    return MOI.VectorAffineFunction(
+        MOI.VectorAffineTerm.(
+            collect(1:length(vars)),
+            MOI.ScalarAffineTerm.(ones(Int, length(vars)), vars),
+        ),
+        zeros(Int, length(vars)),
+    )
+end
+
+end
