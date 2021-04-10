ad4c6a68c5bc801ec4acfdd46f929f1ea82f3951
Thibaut Cuvelier <dourouc05@gmail.com>
2021-04-10 22:35:55 +0200

    Split tests into two files.

---
+++ test/sets.jl
@@ -0,0 +1,39 @@
+@testset "Sets" begin
+    # isbits.
+    @testset "AllDifferent" begin
+        @test isbitstype(CP.AllDifferent)
+
+        @test CP.AllDifferent(2) == CP.AllDifferent(2)
+        @test CP.AllDifferent(2) != CP.AllDifferent(3)
+        @test CP.AllDifferent(3) != CP.AllDifferent(2)
+
+        @test MOI.dimension(CP.AllDifferent(3)) == 3
+    end
+
+    @testset "Strictly{$(Ssub)}" for Ssub in [MOI.LessThan, MOI.GreaterThan]
+        @test CP.Strictly(Ssub(1)) == CP.Strictly(Ssub(1))
+        @test CP.Strictly(Ssub(1)) != CP.Strictly(Ssub(2))
+        @test CP.Strictly(Ssub(2)) == CP.Strictly(Ssub(2))
+        @test CP.Strictly(Ssub(2)) != CP.Strictly(Ssub(1))
+
+        @test MOI.constant(CP.Strictly(Ssub(3))) == 3
+        @test MOI.dimension(CP.Strictly(Ssub(3))) == 1
+        @test MOIU.shift_constant(CP.Strictly(Ssub(3)), 1) == CP.Strictly(Ssub(4))
+    end
+
+    @testset "DifferentFrom" begin
+        @test isbitstype(CP.DifferentFrom{Float64})
+
+        @test CP.DifferentFrom(1) == CP.DifferentFrom(1)
+        @test CP.DifferentFrom(1) != CP.DifferentFrom(2)
+        @test CP.DifferentFrom(2) == CP.DifferentFrom(2)
+        @test CP.DifferentFrom(2) != CP.DifferentFrom(1)
+
+        @test MOI.constant(CP.DifferentFrom(3)) == 3
+        @test MOI.dimension(CP.DifferentFrom(3)) == 1
+        @test MOIU.shift_constant(CP.DifferentFrom(3), 1) == CP.DifferentFrom(4)
+    end
+
+    # Not isbits. Also test copying.
+    # TODO.
+end
