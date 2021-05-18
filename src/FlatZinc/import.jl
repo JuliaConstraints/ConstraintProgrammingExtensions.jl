fbd33e6d9629e307a169e8cbfb243051f05ba379
Thibaut Cuvelier <dourouc05@gmail.com>
2021-05-18 18:24:08 +0200

    Basic architecture for reading FZN files.

--- src/FlatZinc/import.jl
+++ src/FlatZinc/import.jl
@@ -4,7 +4,133 @@
 # =
 # =============================================================================
 
-function Base.read!(::IO, ::Optimizer)
-    error("read! is not implemented for FlatZinc (fzn) files.")
+@enum FznParserstate FznPredicate FznParameter FznVar FznConstraint FznSolve FznDone
+
+const FZN_PARAMETER_TYPES_PREFIX = String["bool", "int", "float", "set of int", "array"]
+
+function Base.read!(io::IO, model::Optimizer)
+    if !MOI.is_empty(model)
+        error("Cannot read in file because model is not empty.")
+    end
+
+    # Start parsing loop.
+    state = FznPredicate
+    while !eof(io)
+        # Consistency check: in FznDone state, nothing else can be read.
+        if state == FznDone
+            break
+        end
+
+        # Read an item from the file.
+        item = get_fzn_item(io)
+        
+        # Skip blank lines.
+        if isempty(item)
+            continue  
+        end
+
+        # Depending on the state, different tokens are expected. Not all 
+        # statees must be reached for all files: only FznSolve is mandatory, 
+        # according to the grammar.
+        if state == FznPredicate
+            if startswith(item, "predicate")
+                parse_predicate!(item, model)
+            else
+                state = FznParameter
+            end
+        end
+
+        if state == FznParameter
+            if any(startswith(item, par_type) for par_type in FZN_PARAMETER_TYPES_PREFIX)
+                parse_parameter!(item, model)
+            else
+                state = FznVar
+            end
+        end
+
+        if state == FznVar
+            if startswith(item, "var")
+                parse_variable!(item, model)
+            else
+                state = FznConstraint
+            end
+        end
+
+        if state == FznConstraint
+            if startswith(item, "constraint")
+                parse_constraint!(item, model)
+            else
+                state = FznSolve
+            end
+        end
+
+        if state == FznSolve
+            if startswith(item, "solve")
+                parse_constraint!(item, model)
+                state = FznDone
+            else
+                error("Syntax error: expected a solve-item.")
+            end
+        end
+    end
+
     return nothing
+end
+
+# -----------------------------------------------------------------------------
+# - High-level parsing functions.
+# -----------------------------------------------------------------------------
+
+function parse_predicate!(item::String, model::Optimizer)
+    error("Predicates are not supported.")
+    return nothing
+end
+
+function parse_parameter!(item::String, model::Optimizer)
+    error("Parameters are not supported.")
+    return nothing
+end
+
+function parse_variable!(item::String, model::Optimizer)
+    error("Variables are not supported.")
+    return nothing
+end
+
+function parse_constraint!(item::String, model::Optimizer)
+    error("Constraints are not supported.")
+    return nothing
+end
+
+function parse_solve!(item::String, model::Optimizer)
+    error("Solves are not supported.")
+    return nothing
+end
+
+# -----------------------------------------------------------------------------
+# - Low-level parsing functions.
+# -----------------------------------------------------------------------------
+
+function get_fzn_item(io::IO)
+    # A FlatZinc item is delimited by a semicolon (;) at the end. Return one 
+    # complete such item, excluding any comments.
+    item = ""
+    while !eof(io)
+        c = read(io, Char)
+
+        # A comment starts with a percent (%) and ends at the end of the line.
+        # Stop reading this character and continue normally at the next line.
+        if c == '%'
+            readline(io)
+            continue
+        end
+
+        # Push the new character into the string.
+        item *= c
+
+        # An item is delimited by a semicolon.
+        if c == '%'
+            break
+        end
+    end
+    return string(strip(item))
 end
