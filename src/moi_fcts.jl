c72bb3af2c96b8ace17aeea9d40f9cd6b987d159
Thibaut Cuvelier <dourouc05@gmail.com>
2021-05-29 05:44:42 +0200

    Nonlinear functions to map CP sets.

--- src/moi_fcts.jl
+++ src/moi_fcts.jl
@@ -247,6 +247,53 @@ function InverseFunction(expr::F) where {F <: NL_SV_FCT}
 end
 
 # -----------------------------------------------------------------------------
+# - Function version of some sets
+# -----------------------------------------------------------------------------
+
+struct CountFunction{F <: NL_SV_FCT, G <: NL_SV_FCT} <: AbstractNonlinearScalarFunction
+    array::Vector{<:F}
+    target::G
+end
+
+struct CountCompareFunction{F <: NL_SV_FCT, G <: NL_SV_FCT} <: AbstractNonlinearScalarFunction
+    array_source::Vector{<:F}
+    array_destination::Vector{<:G}
+end
+
+struct CountDistinctFunction{F <: NL_SV_FCT} <: AbstractNonlinearScalarFunction
+    array::Vector{<:F}
+end
+
+struct ElementFunction{F <: NL_SV_FCT, G <: NL_SV_FCT} <: AbstractNonlinearScalarFunction
+    array::Vector{<:F}
+    index::Vector{<:G}
+end
+
+struct MinimumDistanceFunction{F <: NL_SV_FCT} <: AbstractNonlinearScalarFunction
+    array::Vector{<:F}
+end
+
+struct MaximumDistanceFunction{F <: NL_SV_FCT} <: AbstractNonlinearScalarFunction
+    array::Vector{<:F}
+end
+
+struct MinimumFunction{F <: NL_SV_FCT} <: AbstractNonlinearScalarFunction
+    array::Vector{<:F}
+end
+
+struct MaximumFunction{F <: NL_SV_FCT} <: AbstractNonlinearScalarFunction
+    array::Vector{<:F}
+end
+
+struct ArgumentMinimumFunction{F <: NL_SV_FCT} <: AbstractNonlinearScalarFunction
+    array::Vector{<:F}
+end
+
+struct ArgumentMaximumFunction{F <: NL_SV_FCT} <: AbstractNonlinearScalarFunction
+    array::Vector{<:F}
+end
+
+# -----------------------------------------------------------------------------
 # - Relations with linear/quadratic types of MOI
 # -----------------------------------------------------------------------------
 
