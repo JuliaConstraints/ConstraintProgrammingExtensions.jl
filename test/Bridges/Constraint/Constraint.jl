7641ec247cf6828b1dbad7f2a21968373b5e94f0
Thibaut Cuvelier <dourouc05@gmail.com>
2021-06-05 02:15:04 +0200

    Update Constraint.jl

--- test/Bridges/Constraint/Constraint.jl
+++ test/Bridges/Constraint/Constraint.jl
@@ -1,16 +1,16 @@
 @testset "Constraint bridges" begin
-    # @testset "BinPacking" begin
-    #     include("BinPacking/bp_to_milp.jl")
-    #     include("BinPacking/fixedcapa_to_bp.jl")
-    #     include("BinPacking/fixedcapa_to_varcapa.jl")
-    #     include("BinPacking/varcapa_to_bp.jl")
-    #     include("BinPacking/varcapa_to_milp.jl")
-    # end
+    @testset "BinPacking" begin
+        include("BinPacking/bp_to_milp.jl")
+        include("BinPacking/fixedcapa_to_bp.jl")
+        include("BinPacking/fixedcapa_to_varcapa.jl")
+        include("BinPacking/varcapa_to_bp.jl")
+        include("BinPacking/varcapa_to_milp.jl")
+    end
 
     @testset "Knapsack" begin
-        # include("Knapsack/kp_to_milp.jl")
-        # include("Knapsack/kp_to_varcapa.jl")
-        # include("Knapsack/varcapa_to_milp.jl")
+        include("Knapsack/kp_to_milp.jl")
+        include("Knapsack/kp_to_varcapa.jl")
+        include("Knapsack/varcapa_to_milp.jl")
         include("Knapsack/vkp_to_kp.jl")
         include("Knapsack/varcapav_to_varcapa.jl")
     end
