28ec42e9ca2b1969c23272a7bd74a071128795e9
Thibaut Cuvelier <dourouc05@gmail.com>
2021-06-10 15:35:31 +0200

    Compute lower/upper bounds for affine expressions.

--- src/moi_traits.jl
+++ src/moi_traits.jl
@@ -1,3 +1,7 @@
+# -----------------------------------------------------------------------------
+# - Detect whether a variable or a function has some properties.
+# -----------------------------------------------------------------------------
+
 function is_integer(model::MOI.ModelLike, v::MOI.SingleVariable)
     return is_integer(model, v.variable)
 end
@@ -45,4 +49,69 @@ function has_upper_bound(model::MOI.ModelLike, v::MOI.VariableIndex)
     c_idx =
         MOI.ConstraintIndex{MOI.SingleVariable, MOI.LessThan{Float64}}(v.value)
     return MOI.is_valid(model, c_idx)
+end
+
+# -----------------------------------------------------------------------------
+# - Get some properties about a variable or a function.
+# -----------------------------------------------------------------------------
+
+function get_lower_bound(model::MOI.ModelLike, v::MOI.SingleVariable)
+    return get_lower_bound(model, v.variable)
+end
+
+function get_lower_bound(model::MOI.ModelLike, v::MOI.ScalarAffineFunction)
+    lb = zero(Float64)
+    for t in v.terms
+        var_lb = get_lower_bound(model, t.variable_index)
+        var_ub = get_upper_bound(model, t.variable_index)
+
+        if t.coefficient == 0
+            continue
+        elseif t.coefficient > 0
+            lb += t.coefficient + var_lb
+        else # t.coefficient < 0
+            lb += t.coefficient + var_ub
+        end
+    end
+
+    return lb
+end
+
+function get_lower_bound(model::MOI.ModelLike, v::MOI.VariableIndex)
+    # TODO: not just Float64.
+    c_idx = MOI.ConstraintIndex{MOI.SingleVariable, MOI.GreaterThan{Float64}}(
+        v.value,
+    )
+    set = MOI.get(model, MOI.ConstraintSet(), c_idx)
+    return set.lower
+end
+
+function get_upper_bound(model::MOI.ModelLike, v::MOI.SingleVariable)
+    return get_upper_bound(model, v.variable)
+end
+
+function get_upper_bound(model::MOI.ModelLike, v::MOI.ScalarAffineFunction)
+    ub = zero(Float64)
+    for t in v.terms
+        var_lb = get_lower_bound(model, t.variable_index)
+        var_ub = get_upper_bound(model, t.variable_index)
+
+        if t.coefficient == 0
+            continue
+        elseif t.coefficient > 0
+            ub += t.coefficient + var_ub
+        else # t.coefficient < 0
+            ub += t.coefficient + var_lb
+        end
+    end
+
+    return ub
+end
+
+function get_upper_bound(model::MOI.ModelLike, v::MOI.VariableIndex)
+    # TODO: not just Float64.
+    c_idx =
+        MOI.ConstraintIndex{MOI.SingleVariable, MOI.LessThan{Float64}}(v.value)
+    set = MOI.get(model, MOI.ConstraintSet(), c_idx)
+    return set.upper
 end
