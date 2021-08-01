0f5a2a8c8fff617936dec50313aead6a5eb86378
Thibaut Cuvelier <dourouc05@gmail.com>
2021-08-01 21:10:34 +0200

    Add tests for the new test suite.

---
+++ test/Test/indicator.jl
@@ -0,0 +1,11 @@
+@testset "Indicator" begin
+    mock = MOIU.MockOptimizer(MOIU.UniversalFallback(MOIU.Model{Int}()))
+    config = MOIT.TestConfig()
+
+    MOIU.set_mock_optimize!(
+        mock,
+        (mock::MOIU.MockOptimizer) -> (MOIU.mock_optimize!(mock, [1, 2])),
+        (mock::MOIU.MockOptimizer) -> (MOIU.mock_optimize!(mock, [1, 2])),
+    )
+    COIT.indicatortest(mock, config)
+end
