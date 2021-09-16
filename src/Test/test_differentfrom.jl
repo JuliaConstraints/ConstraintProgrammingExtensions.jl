59a3278dd4b2d91485df7085fc9d2490ed7b5c33
Thibaut Cuvelier <dourouc05@gmail.com>
2021-09-16 23:22:39 +0200

    Create test_differentfrom.jl

---
+++ src/Test/test_differentfrom.jl
@@ -0,0 +1,87 @@
+function test_differentfrom_singlevariable(
+    model::MOI.ModelLike,
+    config::MOIT.Config{T},
+) where {T <: Real}
+    @MOIT.requires MOI.supports_constraint(model, MOI.VariableIndex, MOI.Integer) # x1
+    @MOIT.requires MOI.supports_constraint(model, MOI.VariableIndex, MOI.Interval{T}) # c1
+    @MOIT.requires MOI.supports_constraint(model, MOI.VariableIndex, CP.DifferentFrom{T}) # c2
+
+    x1, _ = MOI.add_constrained_variable(model, MOI.Integer())
+
+    c1 = MOI.add_constraint(model, x1, MOI.Interval(1, 2))
+
+    c2 = MOI.add_constraint(model, x1, CP.DifferentFrom(2))
+
+    @test MOI.is_valid(model, x1)
+    @test MOI.is_valid(model, c1)
+    @test MOI.is_valid(model, c2)
+
+    if MOIT._supports(config, MOI.optimize!)
+        MOI.optimize!(model)
+        @test MOI.get(model, MOI.TerminationStatus()) == MOI.OPTIMAL
+        @test MOI.get(model, MOI.PrimalStatus()) == MOI.FEASIBLE_POINT
+
+        @test MOI.get(model, MOI.ResultCount()) >= 1
+        @test MOI.get(model, MOI.VariablePrimal(), x1) == 1
+    end
+end
+
+function MOIT.setup_test(
+    ::typeof(test_differentfrom_singlevariable),
+    mock::MOIU.MockOptimizer,
+    ::Config{T},
+) where {T <: Real}
+    MOIU.set_mock_optimize!(
+        mock,
+        (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, T[1]),
+    )
+    return
+end
+
+function test_differentfrom_scalaraffinefunction(
+    model::MOI.ModelLike,
+    config::MOIT.Config{T},
+) where {T <: Real}
+    @MOIT.requires MOI.supports_constraint(model, MOI.VariableIndex, MOI.Integer) # x1, x2
+    @MOIT.requires MOI.supports_constraint(model, MOI.VariableIndex, MOI.Interval{T}) # c1, c2
+    @MOIT.requires MOI.supports_constraint(model, MOI.ScalarAffineFunction{T}, CP.DifferentFrom{T}) # c3
+    @MOIT.requires MOI.supports_constraint(model, MOI.ScalarAffineFunction{T}, MOI.EqualTo{T}) # c4
+
+    x1, _ = MOI.add_constrained_variable(model, MOI.Integer())
+    x2, _ = MOI.add_constrained_variable(model, MOI.Integer())
+
+    c1 = MOI.add_constraint(model, x1, MOI.Interval(1, 2))
+    c2 = MOI.add_constraint(model, x2, MOI.Interval(1, 2))
+
+    c3 = MOI.add_constraint(model, 1 * x1 - 1 * x2, CP.DifferentFrom(0))
+    c4 = MOI.add_constraint(model, 1 * x1, MOI.EqualTo(1))
+
+    @test MOI.is_valid(model, x1)
+    @test MOI.is_valid(model, x2)
+    @test MOI.is_valid(model, c1)
+    @test MOI.is_valid(model, c2)
+    @test MOI.is_valid(model, c3)
+    @test MOI.is_valid(model, c4)
+
+    if MOIT._supports(config, MOI.optimize!)
+        MOI.optimize!(model)
+        @test MOI.get(model, MOI.TerminationStatus()) == MOI.OPTIMAL
+        @test MOI.get(model, MOI.PrimalStatus()) == MOI.FEASIBLE_POINT
+
+        @test MOI.get(model, MOI.ResultCount()) >= 1
+        @test MOI.get(model, MOI.VariablePrimal(), x1) == 1
+        @test MOI.get(model, MOI.VariablePrimal(), x2) == 2
+    end
+end
+
+function MOIT.setup_test(
+    ::typeof(test_differentfrom_scalaraffinefunction),
+    mock::MOIU.MockOptimizer,
+    ::Config{T},
+) where {T <: Real}
+    MOIU.set_mock_optimize!(
+        mock,
+        (mock::MOIU.MockOptimizer) -> MOIU.mock_optimize!(mock, T[1, 2]),
+    )
+    return
+end
